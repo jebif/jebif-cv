8c529b3f42fe43b6b56d9e524b0be25a85c734ab
Loïc Paulevé <loic@inzenet.org>
2010-08-28 18:50:56 +0200

    suppr des fichiers racine

--- signals.py
+++
@@ -1,5 +0,0 @@
-
-def create_profile_for_user(sender, **kwargs):
-    from users.models import UserProfile
-    user = kwargs['instance']
-    UserProfile.objects.get_or_create(user = user)
--- urls.py
+++ urls.py
@@ -1,24 +1,15 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
 
-from jebif_cv import settings
-
-admin.autodiscover()
+from jebif import settings
 
 urlpatterns = patterns('',
-    # add 'django.contrib.admindocs' to INSTALLED_APPS 
-    # to enable admin documentation
-    # (r'^admin/doc/',    include('django.contrib.admindocs.urls')),
-    
     (r'^cv/',           include('candidate.urls')),
     (r'^users/',        include('users.urls')),
-    (r'^admin/',        include(admin.site.urls)),
     (r'^accounts/',     include('registration.urls')),
-    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-    (r'^admin_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.ADMIN_MEDIA_ROOT})
 )
-
 urlpatterns += patterns('django.views.generic.simple',
     # Homepage
     (r'', 'direct_to_template', {'template': 'homepage.html'}),
 )
+
